90d269f3f1494b881cf2a544adae7efe92b014ce
saviel <savman1994@gmail.com>
2025-06-27 18:22:10 +0200

    WIP RecipeController endpoints

--- RecipeService/src/main/java/com/example/RecipeService/Controller/RecipeController.java
+++ RecipeService/src/main/java/com/example/RecipeService/Controller/RecipeController.java
@@ -1,17 +1,28 @@
 package com.example.RecipeService.Controller;
 
 import com.example.RecipeService.Model.Recipe;
+import com.example.RecipeService.Service.RecipeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/")
 public class RecipeController {
 
-    @GetMapping("/recipes/")
+    @Autowired
+    private RecipeRepository recipeRepository;
+//
+//    RecipeController(RecipeRepository repo) {
+//        this.recipeRepository = repo;
+//    }
+
+    @GetMapping("/recipes")
     public List<Recipe> getRecipes() {
-        return null;
+        List<Recipe> recipeList = recipeRepository.findAll();
+        return recipeList;
     }
 
     @GetMapping("/recipes/{id}")
@@ -19,13 +30,20 @@ public class RecipeController {
         return null;
     }
 
-    @PostMapping("/recipes/")
-    public List<Recipe> createRecipe(final @RequestBody Recipe recipe) {
-        return null;
+    @PostMapping("/recipes")
+    public Recipe createRecipe(final @RequestBody Recipe recipe) {
+        recipeRepository.save(recipe);
+        return recipe;
     }
 
     @DeleteMapping("/recipes/{id}")
-    public Recipe deleteRecipe(final @PathVariable String id) {
-        return null;
+    public Recipe deleteRecipe(final @PathVariable Long id) {
+        Optional<Recipe> recipe = recipeRepository.findById(id);
+        if (recipe.isPresent()) {
+            recipeRepository.delete(recipe.get());
+            return recipe.get();
+        } else {
+            return null;
+        }
     }
 }
