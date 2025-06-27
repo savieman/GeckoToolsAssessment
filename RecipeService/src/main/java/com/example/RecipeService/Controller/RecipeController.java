880f0665798bdf9d4d06a54cd033f5e77316331a
saviel <savman1994@gmail.com>
2025-06-27 11:28:38 +0200

    Added the general endpoints needed for all the functions

--- RecipeService/src/main/java/com/example/RecipeService/Controller/RecipeController.java
+++ RecipeService/src/main/java/com/example/RecipeService/Controller/RecipeController.java
@@ -1,9 +1,31 @@
 package com.example.RecipeService.Controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.example.RecipeService.Model.Recipe;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping("/")
 public class RecipeController {
 
+    @GetMapping("/recipes/")
+    public List<Recipe> getRecipes() {
+        return null;
+    }
 
+    @GetMapping("/recipes/{id}")
+    public Recipe getRecipe(final @PathVariable String id) {
+        return null;
+    }
+
+    @PostMapping("/recipes/")
+    public List<Recipe> createRecipe(final @RequestBody Recipe recipe) {
+        return null;
+    }
+
+    @DeleteMapping("/recipes/{id}")
+    public Recipe deleteRecipe(final @PathVariable String id) {
+        return null;
+    }
 }
