5e17804180662f7317d98792786233d95e01f31a
saviel <savman1994@gmail.com>
2025-06-30 14:11:56 +0200

    Added exceptionController to prevent internal server errors and return an appropriate http status

---
+++ RecipeService/src/main/java/com/example/RecipeService/Controller/ExceptionController.java
@@ -0,0 +1,42 @@
+package com.example.RecipeService.Controller;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionController {
+
+
+    @ExceptionHandler({EntityNotFoundException.class})
+    public ResponseEntity<Object> handleEntityNotFoundException(final EntityNotFoundException entityNotFoundException) {
+        Response response = new Response();
+        response.status = HttpStatus.NOT_FOUND.name();
+        response.reason = "Entity not found";
+
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(response);
+    }
+
+    class Response {
+        String status;
+        String reason;
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public String getReason() {
+            return reason;
+        }
+
+        public void setReason(String reason) {
+            this.reason = reason;
+        }
+    }
+}
