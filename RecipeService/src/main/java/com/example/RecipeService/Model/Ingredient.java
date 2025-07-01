bd017d6d04819670bd3ed725cb5dbcd79470f659
saviel <savman1994@gmail.com>
2025-07-01 23:30:08 +0200

    Fixed being able to add the same ingredient twice

--- RecipeService/src/main/java/com/example/RecipeService/Model/Ingredient.java
+++ RecipeService/src/main/java/com/example/RecipeService/Model/Ingredient.java
@@ -6,24 +6,24 @@ import org.hibernate.boot.model.internal.BinderHelper;
 @Entity
 public class Ingredient {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @Column(nullable = false)
     private String name;
 
     @ManyToOne
     private Recipe recipe;
 
-    public Ingredient() {
+    // For now I used just "amount" but with a real application this is too generic and units of measure would need to be taken into account like grams or cups etc.
+    @Column(nullable = false)
+    private double amount;
 
-    }
+    public Ingredient() {}
 
     public Ingredient(double amount, String name) {
         this.amount = amount;
         this.name = name;
     }
-
-    // For now I used just "amount" but with a real application this is too generic and units of measure would need to be taken into account like grams or cups etc.
-    @Column(nullable = false)
-    private double amount;
 
     public String getName() {
         return name;
